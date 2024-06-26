15dbb80c2958757586224a35bc0a43fb2e76964c
YuanAaron <1500438364@qq.com>
2024-06-26 18:02:41 +0800

    5-2

---
+++ 5-2/widget.cpp
@@ -0,0 +1,26 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+#include <QDebug>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_pushButton_clicked()
+{
+    int index = 5;
+    for (int i = 0; i < 10; ++i) {
+        qDebug() << "第" << i+1 << "行";
+        index = i;
+    }
+}
+
---
+++ 5-2/widget.h
@@ -0,0 +1,24 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::widget *ui;
+};
+#endif // WIDGET_H
