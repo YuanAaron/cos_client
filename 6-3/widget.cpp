be00e9c61ff8d8e0e7c5645b9fb6967839fea8f7
YuanAaron <1500438364@qq.com>
2024-06-27 10:22:45 +0800

    自带信号，自定义槽

---
+++ 6-3/widget.cpp
@@ -0,0 +1,27 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    connect(ui->spinBox, SIGNAL(valueChanged(int)), this, SLOT(onSpinBoxValueChanged(int)));
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::onSpinBoxValueChanged(int val)
+{
+    ui->progressBar->setValue(val);
+}
+
+void Widget::onNumChanged(int val)
+{
+    ui->progressBar->setValue(val);
+}
+
