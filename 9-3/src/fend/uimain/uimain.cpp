abe46f994ce3c80405d086c7bf3f42b9792ea782
YuanAaron <1500438364@qq.com>
2024-07-02 14:46:46 +0800

    qt项目结构和主子界面以及提升界面、分割器

---
+++ 9-3/src/fend/uimain/uimain.cpp
@@ -0,0 +1,18 @@
+﻿#include "uimain.h"
+#include "ui_uimain.h"
+
+UiMain::UiMain(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::UiMain)
+{
+    ui->setupUi(this);
+
+    //使用分割器后，设置窗口占用的初始比例
+    ui->splitter->setStretchFactor(0,1);
+    ui->splitter->setStretchFactor(1,4);
+}
+
+UiMain::~UiMain()
+{
+    delete ui;
+}
