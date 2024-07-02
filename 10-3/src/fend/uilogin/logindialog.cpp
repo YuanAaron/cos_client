3ff8bd30e6bc09f585064405ddff2c1e00318443
YuanAaron <1500438364@qq.com>
2024-07-02 18:59:58 +0800

    合并登录窗口和主界面代码

---
+++ 10-3/src/fend/uilogin/logindialog.cpp
@@ -0,0 +1,28 @@
+﻿#include "logindialog.h"
+#include "ui_logindialog.h"
+
+LoginDialog::LoginDialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::LoginDialog)
+{
+    ui->setupUi(this);
+
+    //设置窗口无标题栏
+    this->setWindowFlags(Qt::CustomizeWindowHint);
+
+    QPixmap pixmap("C:/Users/oshacker/Documents/qt/10-3/qt.png");
+//    ui->labelLogo->setPixmap(pixmap);
+//    ui->labelLogo->setScaledContents(true);
+    ui->labelLogo->setPixmap(pixmap.scaled(ui->labelLogo->size()));
+}
+
+LoginDialog::~LoginDialog()
+{
+    delete ui;
+}
+
+
+void LoginDialog::on_btnClose_clicked()
+{
+    close();
+}
