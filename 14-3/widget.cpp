e1462f9439ea0f742c967ca1c291a88ea1cbfabb
YuanAaron <1500438364@qq.com>
2024-07-05 23:22:57 +0800

    获取信号发出者

---
+++ 14-3/widget.cpp
@@ -0,0 +1,27 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+#include <QDebug>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    connect(ui->btnDownload, SIGNAL(clicked()), this, SLOT(onButtonClicked()));
+    connect(ui->btnUpload, SIGNAL(clicked()), this, SLOT(onButtonClicked()));
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::onButtonClicked()
+{
+    //获取信号发出者及文本
+    QPushButton* btn = dynamic_cast<QPushButton*>(sender());
+    qDebug() << btn->text();
+}
+
