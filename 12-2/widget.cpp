2e4ccc5d18feab55d261ecc4e474b6f4dc3aa5b8
YuanAaron <1500438364@qq.com>
2024-07-06 18:56:40 +0800

    自作相框

---
+++ 12-2/widget.cpp
@@ -0,0 +1,30 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+#include <QFileDialog>
+#include <QDebug>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_pushButton_clicked()
+{
+    QString path = QFileDialog::getOpenFileName(this, QString::fromLocal8Bit("上传"), "./");
+    if(path != "")
+    {
+        qDebug() << path;
+        QString style = "border-image:url(\"" + path + "\");";
+        qDebug() << style;
+        ui->frame_2->setStyleSheet(style);
+    }
+}
