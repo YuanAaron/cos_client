9f4c493c6952f80d82453a9cf5951921c6d481f9
YuanAaron <1500438364@qq.com>
2024-07-09 16:35:25 +0800

    MVC思想

---
+++ 15-2/widget.cpp
@@ -0,0 +1,33 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+#include <QFileSystemModel>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    ui->splitter->setStretchFactor(0,1);
+    ui->splitter->setStretchFactor(1,4);
+    ui->splitter->setStretchFactor(2,2);
+
+    QFileSystemModel* model = new QFileSystemModel(this);
+    model->setRootPath(QDir::currentPath());
+
+    ui->listView->setModel(model);
+    ui->listView->setRootIndex(model->index(QDir::currentPath()));
+
+    ui->tableView->setModel(model);
+    ui->tableView->setRootIndex(model->index(QDir::currentPath()));
+
+    ui->treeView->setModel(model);
+    ui->treeView->setRootIndex(model->index(QDir::currentPath()));
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
