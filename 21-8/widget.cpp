56f19e4137826177e145b3b4feb2741d2b7faeb6
YuanAaron <1500438364@qq.com>
2024-08-09 12:56:27 +0800

    QStackedWidget 栈布局

---
+++ 21-8/widget.cpp
@@ -0,0 +1,24 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_pushButton_clicked()
+{
+    //实现页面循环播放
+    int count = ui->stackedWidget->count();
+    int idx = ui->stackedWidget->currentIndex();
+    ui->stackedWidget->setCurrentIndex((idx+1)%count);
+}
