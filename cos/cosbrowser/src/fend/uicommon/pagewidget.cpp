dd885a83b2dee48a1292b7ae1bd290c63fd3471a
YuanAaron <1500438364@qq.com>
2024-08-09 12:38:09 +0800

    翻页组件

--- cos/cosbrowser/src/fend/uicommon/pagewidget.cpp
+++ cos/cosbrowser/src/fend/uicommon/pagewidget.cpp
@@ -2,16 +2,152 @@
 #include "ui_pagewidget.h"
 
 #include <QDebug>
+#include <QtMath>
 
 PageWidget::PageWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::PageWidget)
 {
     ui->setupUi(this);
+
+    connect(ui->btnNext, &QPushButton::clicked, this, &PageWidget::next);
+    connect(ui->btnPre, &QPushButton::clicked, this, &PageWidget::pre);
+
+    ui->lineMaxRows->setAlignment(Qt::AlignHCenter);
+    ui->lineMaxRows->setReadOnly(true);
+    connect(ui->lineMaxRows, &ComboLine::itemSelected, this, &PageWidget::onMaxRowsItemSelected);
+    ui->lineCurrentPage->setAlignment(Qt::AlignHCenter);
+    ui->lineCurrentPage->setReadOnly(true);
+    connect(ui->lineCurrentPage, &ComboLine::itemSelected, this, &PageWidget::onCurrentPageSelected);
+
+    ui->btnNext->setDisabled(true);
+    ui->btnPre->setDisabled(true);
 }
 
 PageWidget::~PageWidget()
 {
     delete ui;
     qDebug() << "delete PageWidget";
+}
+
+int PageWidget::totalRow() const
+{
+    return m_totalRow;
+}
+
+int PageWidget::maxRow() const
+{
+    return m_maxRow;
+}
+
+int PageWidget::currentPage() const
+{
+    return m_currentPage;
+}
+
+int PageWidget::pageCount() const
+{
+    if(m_totalRow == 0)
+        return 1;
+
+    //qCeil获取不小于value的最小整数
+    return qCeil(m_totalRow/float(m_maxRow));
+}
+
+int PageWidget::isFirstPage() const
+{
+    return currentPage() == 1;
+}
+
+int PageWidget::isLastPage() const
+{
+    return currentPage() == pageCount();
+}
+
+void PageWidget::setTotalRow(int newTotalRow)
+{
+    m_totalRow = newTotalRow;
+    ui->labelTotalCount->setText(QString::fromLocal8Bit("共%1条，每页").arg(newTotalRow));
+    reset();
+}
+
+void PageWidget::setMaxRow(int rows)
+{
+    QStringList words = ui->lineMaxRows->getWords();
+    for(const QString& num: qAsConst(words))
+    {
+        if(num.toInt() == rows)
+        {
+            m_maxRow = rows;
+            reset();
+            break;
+        }
+    }
+}
+
+void PageWidget::setMaxRowList(const QList<int> rowList)
+{
+    ui->lineMaxRows->setWords(rowList);
+    if (rowList.size() > 0) {
+        ui->lineMaxRows->setText(QString::number(rowList[0]));
+        setMaxRow(rowList[0]);
+    }
+}
+
+void PageWidget::next()
+{
+    jumpTo((currentPage()+1));
+}
+
+void PageWidget::pre()
+{
+    jumpTo((currentPage()-1));
+}
+
+void PageWidget::jumpTo(int page)
+{
+    if(page != currentPage())
+    {
+        switchPage(page);
+    }
+}
+
+void PageWidget::onCurrentPageSelected(const QString &text)
+{
+    if (m_currentPage != text.toInt())
+        jumpTo(text.toInt());
+}
+
+void PageWidget::onMaxRowsItemSelected(const QString &text)
+{
+    setMaxRow(text.toInt());
+}
+
+void PageWidget::reset()
+{
+    setPageCount();
+    switchPage(1); //重置为首页
+}
+
+void PageWidget::setPageCount()
+{
+    int count = pageCount();
+    ui->labelPageCount->setText("/"+QString::number(count));
+    ui->lineCurrentPage->setWords(1, count);
+    ui->lineCurrentPage->setText("1");
+}
+
+void PageWidget::switchPage(int newCurrentPage)
+{
+    m_currentPage = newCurrentPage;
+    ui->lineCurrentPage->setText(QString::number(newCurrentPage));
+    ui->btnPre->setDisabled(isFirstPage());
+    ui->btnNext->setDisabled(isLastPage());
+
+    int start = (newCurrentPage-1)*maxRow();
+    int left = totalRow()-start;
+    int len = left < maxRow()?left:maxRow();
+    qDebug() << QString("switchPage %1,%2").arg(start).arg(len);
+    emit pageNumChanged(start, len);
+
 }
--- cos/cosbrowser/src/fend/uicommon/pagewidget.h
+++ cos/cosbrowser/src/fend/uicommon/pagewidget.h
@@ -1,4 +1,4 @@
-#ifndef PAGEWIDGET_H
+﻿#ifndef PAGEWIDGET_H
 #define PAGEWIDGET_H
 
 #include <QWidget>
@@ -15,8 +15,46 @@ public:
     explicit PageWidget(QWidget *parent = nullptr);
     ~PageWidget();
 
+    //获取属性值
+    int totalRow() const;
+    int maxRow() const;
+    int currentPage() const;
+    int pageCount() const;
+    int isFirstPage() const;
+    int isLastPage() const;
+
+    //设置属性值
+    void setTotalRow(int newTotalRow);
+    void setMaxRow(int rows); //设置当前页最大显示行数
+    void setMaxRowList(const QList<int> rowList);   //设置单页显示行数选择列表(初始化部分）
+
+signals:
+    /**
+     * @brief pageNumChanged 当页码发生变化时发出信号
+     * @param start 起始索引
+     * @param maxLen 每页最大条数
+     */
+    void pageNumChanged(int start, int maxLen);
+
+public slots:
+    void next();
+    void pre();
+    void jumpTo(int page);
+
+private slots:
+    void onCurrentPageSelected(const QString &text);
+    void onMaxRowsItemSelected(const QString &text);
+
+private:
+    void reset();                           // 重置为首页
+    void setPageCount();                    // 设置页号选择列表(初始化部分）
+    void switchPage(int newCurrentPage);    // 切换页码
+
 private:
     Ui::PageWidget *ui;
+    int m_totalRow = 0; //总行数
+    int m_maxRow = 10; //每页多少行
+    int m_currentPage = 1; //当前是第几页
 };
 
 #endif // PAGEWIDGET_H
